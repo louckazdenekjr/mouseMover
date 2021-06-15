e818082cb56eb1a8b318b29de9b806555fd47238
Loucka <zdenek.loucka@siemens.com>
2021-06-15 13:07:24 +0200

    changed interval and added error handling

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pyautogui
+import time
 
 while True:
-    pyautogui.moveRel(-20, 0)
-    pyautogui.moveRel(20, 0)
+    try:
+        pyautogui.moveRel(-10, 0)
+        pyautogui.moveRel(10, 0)
+        time.sleep(2)
+    except:
+        print("An exception occurred")
